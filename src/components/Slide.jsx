dc584e51315696c1a437cbcbcbbdd667acfda077
Yaswitha Boppana <323103210032.yaswitha@gvpcew.ac.in>
2025-10-16 10:07:17 +0530

    First slide v1

---
+++ src/components/Slide.jsx
@@ -0,0 +1,30 @@
+import { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import './Slide.css';
+
+const Slide = ({ id, children, backgroundColor = '#ffffff' }) => {
+  const slideRef = useRef(null);
+
+  useEffect(() => {
+    // This could be used for slide-specific initialization
+  }, []);
+
+  return (
+    <div 
+      id={id} 
+      className="slide" 
+      ref={slideRef}
+      style={{ backgroundColor }}
+    >
+      {children}
+    </div>
+  );
+};
+
+Slide.propTypes = {
+  id: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  backgroundColor: PropTypes.string
+};
+
+export default Slide;
